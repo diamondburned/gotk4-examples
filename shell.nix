798972cd6adc7022cc52929fbfb52f141daaec02
diamondburned <diamond@arikawa-hi.me>
2021-12-25 22:07:11 -0800

    Fix up nix-shell

--- shell.nix
+++ shell.nix
@@ -1,27 +1,36 @@
 { systemPkgs ? import <nixpkgs> {} }:
 
 let unstable = import (systemPkgs.fetchFromGitHub {
-	owner  = "NixOS";
-	repo   = "nixpkgs";
-	rev    = "fbfb79400a08bf754e32b4d4fc3f7d8f8055cf94";
-	sha256 = "0pgyx1l1gj33g5i9kwjar7dc3sal2g14mhfljcajj8bqzzrbc3za";
-}) {
-	overlays = [
-		(self: super: {
-			go = super.go.overrideAttrs (old: {
-				version = "1.17beta1";
-				src = builtins.fetchurl {
-					url    = "https://golang.org/dl/go1.17rc1.linux-arm64.tar.gz";
-					sha256 = "sha256:0kps5kw9yymxawf57ps9xivqrkx2p60bpmkisahr8jl1rqkf963l";
-				};
-				doCheck = false;
-			});
-		})
-	];
-};
+		owner  = "NixOS";
+		repo   = "nixpkgs";
+		rev    = "fbfb79400a08bf754e32b4d4fc3f7d8f8055cf94";
+		sha256 = "0pgyx1l1gj33g5i9kwjar7dc3sal2g14mhfljcajj8bqzzrbc3za";
+	}) {
+		overlays = [
+			(self: super: {
+				go = super.go.overrideAttrs (old: {
+					version = "1.17";
+					src = builtins.fetchurl {
+						url    = "https://golang.org/dl/go1.17.linux-amd64.tar.gz";
+						sha256 = "sha256:0b9p61m7ysiny61k4c0qm3kjsjclsni81b3yrxqkhxmdyp29zy3b";
+					};
+					doCheck = false;
+				});
+			})
+		];
+	};
 
-in unstable.mkShell {
-	buildInputs = with unstable; [
+	choosePkgs = system: if (system) then systemPkgs else unstable;
+	lib = systemPkgs.lib;
+
+	goPkgs = choosePkgs
+		((systemPkgs.go or null) != null && lib.versionAtLeast systemPkgs.go.version "1.17");
+
+	gtkPkgs = choosePkgs
+		((systemPkgs.gtk4 or null) != null && lib.versionAtLeast systemPkgs.gtk4.version "4.4.0");
+
+in systemPkgs.mkShell {
+	buildInputs = with gtkPkgs; [
 		glib
 		graphene
 		gdk-pixbuf
@@ -30,19 +39,16 @@ in unstable.mkShell {
 		vulkan-headers
 	];
 
-	nativeBuildInputs =
-		with unstable;
-		let sh = unstable.writeShellScriptBin;
-		in [
-			# Build/generation dependencies.
-			gobjectIntrospection
-			pkgconfig
-			go
+	nativeBuildInputs = with systemPkgs; [
+		# Build/generation dependencies.
+		gobjectIntrospection
+		pkgconfig
+		goPkgs.go
 
-			# Development tools.
-			# gopls
-			goimports
-		];
+		# Development tools.
+		# gopls
+		# goimports
+	];
 
 	CGO_ENABLED = "1";
 
