f0deba8b85331c75165cccaed29377e830fdd583
Erazem Kokot <mail@erazem.eu>
2022-03-21 12:44:23 +0100

    Add a builder example
    This commit adds an example using Builder, along with an example Cambalache
    project file for easier learning and improving.

---
+++ gtk4/builder/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+	"strconv"
+
+	_ "embed"
+
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+)
+
+//go:embed main.ui
+var uiXML string
+
+func main() {
+	app := gtk.NewApplication("com.github.diamondburned.gotk4-examples.gtk4.builder", 0)
+	app.ConnectActivate(func() { activate(app) })
+
+	if code := app.Run(os.Args); code > 0 {
+		os.Exit(code)
+	}
+}
+
+func activate(app *gtk.Application) {
+	// You can build UIs using Cambalache (https://flathub.org/apps/details/ar.xjuan.Cambalache)
+	builder := gtk.NewBuilderFromString(uiXML, len(uiXML))
+
+	// MainWindow and Button are object IDs from the UI file
+	window := builder.GetObject("MainWindow").Cast().(*gtk.Window)
+	button := builder.GetObject("Button").Cast().(*gtk.Button)
+
+	counter := 0
+	button.Connect("clicked", func() {
+		button.SetLabel(strconv.Itoa(counter))
+		counter++
+	})
+
+	app.AddWindow(window)
+	window.Show()
+}
